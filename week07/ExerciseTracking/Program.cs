6d091c81e21f38a21b86b3296e4f1d0b0e44a228
DiegoHC <alask27echufles@gmail.com>
2025-04-16 07:35:01 -0500

    W07 Assignment: Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var activities = new List<Activity>{
+            new Running(new DateTime(2025, 03, 4), 30, 3.0),
+            new Cycling(new DateTime(2025, 02, 5), 30, 9.7),
+            new Swimming(new DateTime(2025, 01, 6), 30, 40)
+        };
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSumary());
+        }
     }
 }
